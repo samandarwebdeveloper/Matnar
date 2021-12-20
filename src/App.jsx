7d72d0c08fde05bd6e85f13bbd315d71081e9bb6
Roziboy <roziqov1011@gmail.com>
2021-12-20 16:51:14 +0500

    Revert "Proba"

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { Route, Switch  } from "react-router";
 // components
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
-import Product from "./pages/Product/Product";
 
 function App() {
   return (
@@ -15,13 +14,6 @@ function App() {
       <Header />
       <Switch>
         <Route path="/" exact><Home /></Route>
-        
-        <Route path="/vomen"><Product /></Route>
-
-        <Route path="/men"><Product /></Route>
-
-        <Route path="/kids"><Product /></Route>
-        
       </Switch>
     </div>
   );
